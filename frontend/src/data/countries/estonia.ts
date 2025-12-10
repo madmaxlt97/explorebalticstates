2ab6a345962067b7134e265416f6f50506129215
Max <madmaxlt97@gmail.com>
2025-12-10 18:58:23 +0200

    changed topics code to avoid hardcoding

--- frontend/src/data/countries/estonia.ts
+++ frontend/src/data/countries/estonia.ts
@@ -2,96 +2,53 @@ import type { City } from "../../types/CityType";
 
 export const estoniaCities: City[] = [
   {
-    id: "vilnius",
-    countryId: "lithuania",
-    name: "Vilnius",
+    id: "tallinn",
+    countryId: "estonia",
+    name: "Tallinn",
     image: "",
     description: "",
     funFacts: [],
     topics: [
       {
-        id: "vilnius-airport",
-        title: "Best way to get from airport to Vilnius center",
+        id: "tallinn-airport",
+        title: "Best way to get from airport to Tallinn center",
         cardImage: "/assets/images/topics-images/airport-topic-img.jpg",
       },
       {
-        id: "vilnius-hotels",
-        title: "Best areas to stay in Vilnius",
+        id: "tallinn-hotels",
+        title: "Best areas to stay in Tallinn",
         cardImage: "/assets/images/topics-images/hotel-topic-img.jpg",
       },
       {
-        id: "vilnius-cuisine",
+        id: "tallinn-cuisine",
         title: "Best places to try national cuisine in Vilnius",
         cardImage: "/assets/images/topics-images/cuisine-topic-img.jpg",
       },
-      {
-        id: "vilnius-new-year-activities",
-        title:
-          "A Winter Fairytale: Your Ultimate New Year's Getaway to Vilnius",
-        cardImage: "/assets/images/topics-images/airport-topic-img.jpg",
-      },
     ],
   },
   {
-    id: "kaunas",
-    countryId: "lithuania",
-    name: "Kaunas",
+    id: "tartu",
+    countryId: "estonia",
+    name: "Tartu",
     image: "",
     description: "",
     funFacts: [],
     topics: [
       {
-        id: "kaunas-airport",
-        title: "Best way to get from airport to Kaunas center",
+        id: "tartu-airport",
+        title: "Best way to get from airport to Tartu center",
         cardImage: "/assets/images/topics-images/airport-topic-img.jpg",
       },
       {
-        id: "kaunas-hotels",
-        title: "Best areas to stay in Kaunas",
+        id: "tartu-hotels",
+        title: "Best areas to stay in Tartu",
         cardImage: "/assets/images/topics-images/hotel-topic-img.jpg",
       },
       {
-        id: "kaunas-cuisine",
-        title: "Best places to try national cuisine in Kaunas",
+        id: "tartu-cuisine",
+        title: "Best places to try national cuisine in Tartu",
         cardImage: "/assets/images/topics-images/cuisine-topic-img.jpg",
       },
-      /*{
-        id: "vilnius-new-year-activities",
-        title:
-          "A Winter Fairytale: Your Ultimate New Year's Getaway to Vilnius",
-        cardImage: "/assets/images/topics-images/airport-topic-img.jpg",
-      },*/
-    ],
-  },
-  {
-    id: "klaipeda",
-    countryId: "lithuania",
-    name: "Klaipeda",
-    image: "",
-    description: "",
-    funFacts: [],
-    topics: [
-      {
-        id: "klaipeda-airport",
-        title: "Best way to get from airport to Klaipeda center",
-        cardImage: "/assets/images/topics-images/airport-topic-img.jpg",
-      },
-      {
-        id: "klaipeda-hotels",
-        title: "Best areas to stay in Klaipeda",
-        cardImage: "/assets/images/topics-images/hotel-topic-img.jpg",
-      },
-      {
-        id: "klaipeda-cuisine",
-        title: "Best places to try national cuisine in Klaipeda",
-        cardImage: "/assets/images/topics-images/cuisine-topic-img.jpg",
-      },
-      /*{
-        id: "vilnius-new-year-activities",
-        title:
-          "A Winter Fairytale: Your Ultimate New Year's Getaway to Vilnius",
-        cardImage: "/assets/images/topics-images/airport-topic-img.jpg",
-      },*/
     ],
   },
 ];
